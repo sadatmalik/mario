4d2bb5d7f41109a77e369c852cadeec6617c6320
sadat.malik <sadat.malik@me.com>
2022-01-10 10:11:53 +0000

    Implementing key listener

---
+++ src/main/java/jade/KeyListener.java
@@ -0,0 +1,32 @@
+package jade;
+
+import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
+import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+
+public class KeyListener {
+    private static KeyListener instance;
+    private boolean keyPressed[] = new boolean[350]; //GLFW keybindings = 350
+
+    private KeyListener() {
+
+    }
+
+    public static KeyListener get() {
+        if (instance == null) {
+            instance = new KeyListener();
+        }
+        return instance;
+    }
+
+    public static void keyCallback(long window, int key, int scancode, int action, int modifiers) {
+        if (action == GLFW_PRESS) {
+            get().keyPressed[key] = true;
+        } else if (action == GLFW_RELEASE) {
+            get().keyPressed[key] = false;
+        }
+    }
+
+    public static boolean isKeyPressed(int keyCode) {
+        return get().keyPressed[keyCode];
+    }
+}
